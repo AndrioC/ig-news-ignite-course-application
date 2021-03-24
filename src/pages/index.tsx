19876aa759cca90b2d61ca3ca3dc7cd0a68d352f
andrioc <andrio@rodrigo.silva@hotmail.com>
2021-03-24 14:36:26 -0300

    Consuming Stripe API to show price amount

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps{
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
     <Head>
@@ -15,9 +24,9 @@ export default function Home() {
         <h1>News about he <span>React</span> world.</h1>
         <p>
           Get access to all the publications <br />
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId}/>
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding"/>
@@ -27,4 +36,25 @@ export default function Home() {
 
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1IYaHTJoR8mO0VUJxxp0bjXg')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format((price.unit_amount / 100))
+  }
+
+  return {
+    props: {
+      product
+    }
+  }
+  
 }
