69417188c0b7da31035d0219a8072aaf2953552b
andrioc <andrio@rodrigo.silva@hotmail.com>
2021-03-24 14:45:29 -0300

    Changing from SSR to SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -28,10 +28,7 @@ export default function Home({product}: HomeProps) {
         </p>
         <SubscribeButton priceId={product.priceId}/>
       </section>
-
-      <img src="/images/avatar.svg" alt="Girl coding"/>
-
-      
+      <img src="/images/avatar.svg" alt="Girl coding"/>    
     </main>
 
     </>
@@ -39,7 +36,7 @@ export default function Home({product}: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1IYaHTJoR8mO0VUJxxp0bjXg')
 
@@ -54,7 +51,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
   
 }
